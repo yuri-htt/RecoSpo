796d874fe2e53fe7b381d0a1bddabbfafcc1d3cb
YuriHotate <yuri.swift.615@gmail.com>
2017-08-27 17:29:46 +0900

    rebuild navigation

--- src/components/addEvent.js
+++ src/components/addEvent.js
@@ -5,11 +5,34 @@ import React, { Component } from 'react';
 import {
     Image,
     Text,
+    View,
+    StyleSheet,
 } from 'react-native';
   
 export default class AddEvent extends Component {
-
     render() {
-        <Text> add event here</Text>
+        return (
+            <View style={styles.scene}>
+                <View style={styles.centering}>
+                    <Text style={styles.txt}>AddEvent</Text>
+                </View>
+            </View>   
+        );
     }
 }
+
+var styles = StyleSheet.create({
+    scene: {
+        flex: 1,
+        backgroundColor: 'blue',
+    },
+    centering: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    txt: {
+        color: 'white',
+        fontSize: 20,
+    },
+})
--- src/components/search.js
+++ src/components/search.js
@@ -4,13 +4,37 @@ import React, { Component } from 'react';
 
 import {
     Image,
+    View,
+    Text,
+    StyleSheet,
 } from 'react-native';
   
 const entryImg = require('../img/entry.jpg');
 
 export default class Search extends Component {
-
     render() {
-        <Image source={entryImg} />
+        return (
+            <View style={styles.scene}>
+                <View style={styles.centering}>
+                    <Text style={styles.txt}>Search</Text>
+                </View>
+            </View>   
+        );
     }
 }
+
+var styles = StyleSheet.create({
+    scene: {
+        flex: 1,
+        backgroundColor: 'green',
+    },
+    centering: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    txt: {
+        color: 'white',
+        fontSize: 20,
+    },
+})
