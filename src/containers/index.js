dfdd6eb37421d64ae145290c2f807426fab7f45d
Yuri Hotate <yuri.swift.615@gmail.com>
2017-09-04 14:44:37 +0900

    UI of sign up

--- src/containers/index.js
+++ src/containers/index.js
@@ -6,9 +6,19 @@ import {
   Text,
   Dimensions,
   StyleSheet,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
+import {
+  Divider,
+} from 'react-native-material-design';
 import Modal from 'react-native-modalbox';
+import {
+  KeyboardAwareScrollView,
+} from 'react-native-keyboard-aware-scroll-view';
+
 import { Tabs } from './router';
+import config from '../lib/config';
 
 const signUpImg = require('../img/signUp.jpg');
 const Realm = require('realm');
@@ -18,6 +28,7 @@ const screenHeight = Dimensions.get('window').height;
 export default class App extends Component {
   state = {
     modalVisible: false,
+    text: 'Your name',
   }
 
   componentWillMount() {
@@ -50,7 +61,30 @@ export default class App extends Component {
         isOpen={this.state.modalVisible}
         onClosed={() => this.setState({modalVisible: false})}
       >
-        <Image source={signUpImg} style={styles.backGroundImg}/>
+        <Image source={signUpImg} style={styles.backGroundImg}>
+          <KeyboardAwareScrollView
+            style={styles.scene}
+            keyboardShouldPersistTaps="handled"
+          >
+            <View style={styles.whileScreen}>
+
+              <View style={styles.textInputContainer}>
+                <TextInput
+                  style={styles.textInput}
+                  onChangeText={(text) => this.setState({text})}
+                  value={this.state.text}
+                />
+                <Divider style={styles.border}/>
+              </View>
+
+              <TouchableOpacity style={styles.signUpBtn}>
+                <Text style={styles.signUpTxt}>Sign Up</Text>
+              </TouchableOpacity>
+
+            </View>
+
+          </KeyboardAwareScrollView>
+        </Image>
       </Modal>
     )
   }
@@ -61,9 +95,10 @@ var styles = StyleSheet.create({
       flex: 1,
   },
   centering: {
-      flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
+      flex:1,
+      backgroundColor: 'blue',
   },
   backGroundImg: {
       position: 'absolute',
@@ -71,6 +106,41 @@ var styles = StyleSheet.create({
       left: 0,
       width : screenWidth,
       height: screenHeight,
+  },
+  whileScreen: {
+    width : screenWidth,
+    height: screenHeight,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInputContainer: {
+    height:44,
+    width: 300,
+    marginHorizontal: 20,
+  },
+  textInput: {
+    height:44,
+    marginLeft:10,
+    color: 'white',
+    fontSize: 26,
+  },
+  border: {
+    backgroundColor: 'white',
+  },
+  signUpBtn: {
+    position: 'absolute',
+    bottom: 100,
+    width: 200,
+    height: 40,
+    borderRadius:20,
+    backgroundColor: config.blueColor,
+  },
+  signUpTxt: {
+    color: 'white',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    lineHeight: 40,
+    backgroundColor: config.transparent,
   },
 })
 
--- src/lib/config.js
+++ src/lib/config.js
@@ -3,6 +3,7 @@
 const config = module.exports = {};
 
 config.mainColor = 'rgba(70, 165, 135, 1)';
+config.blueColor = 'rgba(55, 62, 103, 1)';
 config.subColor = 'rgba(155, 190, 180, 1)';
 config.primaryText = 'rgba(0, 0, 0, 0.87)';
 config.secondaryText = 'rgba(0, 0, 0, 0.54)';
