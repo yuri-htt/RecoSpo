d318e919b55ae1b226bf7e78817b963bf8e1d6af
Yuri Hotate <yuri.swift.615@gmail.com>
2017-09-04 13:01:10 +0900

    transition to registration screen in case of unregistered user

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,11 +1,91 @@
 
 import React, { Component } from 'react';
+import {
+  Image,
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
+import Modal from 'react-native-modalbox';
 import { Tabs } from './router';
 
-class App extends Component {
+const signUpImg = require('../img/signUp.jpg');
+const Realm = require('realm');
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
+
+export default class App extends Component {
+  state = {
+    modalVisible: false,
+  }
+
+  componentWillMount() {
+    Realm.open({
+      schema: [NoUser]//[User]
+    }).then(realm => {
+      console.log(realm)
+      // 名前を取得
+      this.setState({modalVisible: false});
+    }).catch(error => {
+      console.log('データがないよ！');
+      this.setState({modalVisible: true})
+    });
+  }
+
   render() {
-    return <Tabs />;
+    return (
+
+    <View style={styles.scene}>
+      {this.renderRegisterModal()}
+      <Tabs />
+    </View>
+    )
+  }
+
+  renderRegisterModal() {
+    return (
+      <Modal
+        style={styles.modal}
+        isOpen={this.state.modalVisible}
+        onClosed={() => this.setState({modalVisible: false})}
+      >
+        <Image source={signUpImg} style={styles.backGroundImg}/>
+      </Modal>
+    )
   }
 }
 
-export default App;
+var styles = StyleSheet.create({
+  scene: {
+      flex: 1,
+  },
+  centering: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  backGroundImg: {
+      position: 'absolute',
+      top:0,
+      left: 0,
+      width : screenWidth,
+      height: screenHeight,
+  },
+})
+
+class User {}
+User.schema = {
+    name: 'User',
+    properties: {
+        name: { type: 'string'},
+    }
+};
+
+class NoUser {}
+User.schema = {
+    name: 'NoUser',
+    properties: {
+        name: { type: 'string'},
+    }
+};
